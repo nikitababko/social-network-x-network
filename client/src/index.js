778fcdb9eb2f34639b412e20097d3bf04c20e244
Nikita Babko <niki.babko@gmail.com>
2021-05-29 16:24:12 +0700

    Imported store provider from redux.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DataProvider from './redux/store';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
