8cd647802cc6f79b8915e722ced397451ddf885f
Nikita Babko <niki.babko@gmail.com>
2021-05-29 16:23:11 +0700

    Import and render Login component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PageRender from './PageRender';
+import Home from './pages/home';
+import Login from './pages/login';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
+          <Route exact path="/" component={Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
