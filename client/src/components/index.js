3c1812296dfafd8255ffe781581185773bf86026
Nikita Babko <niki.babko@gmail.com>
2021-05-30 10:51:24 +0700

    Exported Alert, Loading and Toast component.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as NotFound } from './NotFound';
+export { default as Alert } from './Alert';
+export { default as Loading } from './Loading';
+export { default as Toast } from './Toast';
