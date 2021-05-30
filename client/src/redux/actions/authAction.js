5e5e113cb9c1b306a1ac931a106231066f466530
Nikita Babko <niki.babko@gmail.com>
2021-05-30 10:53:31 +0700

    Added refreshToken method and replaced path to types.

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -1,40 +1,71 @@
+import { GLOBALTYPES } from './globalTypes';
 import { postDataAPI } from 'utils/fetchData';
-
-export const TYPES = {
-  AUTH: 'AUTH',
-};
 
 export const login = (data) => async (dispatch) => {
   try {
     dispatch({
-      type: 'NOTIFY',
+      type: GLOBALTYPES.ALERT,
       payload: {
         loading: true,
       },
     });
     const res = await postDataAPI('login', data);
-    console.log(res);
     dispatch({
-      type: 'AUTH',
+      type: GLOBALTYPES.AUTH,
       payload: {
         token: res.data.access_token,
         user: res.data.user,
       },
     });
 
-    localStorage.setItem('access_token', res.data.access_token);
+    localStorage.setItem('firstLogin', true);
     dispatch({
-      type: 'NOTIFY',
+      type: GLOBALTYPES.ALERT,
       payload: {
         success: res.data.message,
       },
     });
   } catch (error) {
     dispatch({
-      type: 'NOTIFY',
+      type: GLOBALTYPES.ALERT,
       payload: {
         error: error.response.data.message,
       },
     });
+  }
+};
+
+export const refreshToken = () => async (dispatch) => {
+  const firstLogin = localStorage.getItem('firstLogin');
+  if (firstLogin) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        loading: true,
+      },
+    });
+
+    try {
+      const res = await postDataAPI('refresh_token');
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          token: res.data.access_token,
+          user: res.data.user,
+        },
+      });
+
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {},
+      });
+    } catch (error) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          error: error.response.data.message,
+        },
+      });
+    }
   }
 };
