a14273da3253510f81a75445e27e65d4633d2f19
Nikita Babko <niki.babko@gmail.com>
2021-05-30 10:47:34 +0700

    Imported Alert component. Added conditions for Home/Login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import PageRender from './PageRender';
 import Home from './pages/home';
 import Login from './pages/login';
+import { Alert } from './components';
+import { refreshToken } from './redux/actions/authAction';
 
 const App = () => {
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
     <Router>
+      <Alert />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
